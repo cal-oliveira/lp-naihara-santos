6e156173455b2f396d43d87a73882d750d1d5d7e
cal-oliveira <calebe81899026@gmail.com>
2025-09-06 18:33:41 -0300

    fix: remove logo

--- src/app/sections/Header.tsx
+++ src/app/sections/Header.tsx
@@ -21,9 +21,9 @@ const Header = () => {
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <div className="flex items-center space-x-2">
-            <div className="w-8 h-8 bg-gradient-primary rounded-lg flex items-center justify-center">
+            {/* <div className="w-8 h-8 bg-gradient-primary rounded-lg flex items-center justify-center">
               <Heart className="w-5 h-5 text-white" />
-            </div>
+            </div> */}
             <div>
               <h3 className="font-bold text-foreground">Naihara Santos</h3>
               <p className="text-xs text-muted-foreground">Fisioterapeuta</p>
